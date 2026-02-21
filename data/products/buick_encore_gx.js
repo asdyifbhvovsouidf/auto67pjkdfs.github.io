bedfcd604f416276b8a9f7a95bcb52f78141d6e8
asdyifbhvovsouidf <bvjh56944@gmail.com>
2026-02-22 00:20:37 +0300

    Add files via upload

---
+++ data/products/buick_encore_gx.js
@@ -0,0 +1,14 @@
+window['PRODUCTS_BUICK_ENCORE_GX'] = [
+    {
+        "type": "свечи",
+        "brand": "ACDelco",
+        "art": "12688094",
+        "price": 0,
+        "desc": "Buick encore gx 1.3\nChevrolet malibu 1.3\nChevrolet trailblaizer 1.3\nChevrolet trax 1.3\nOpel Astra K 1.3",
+        "images": [
+            "12688094_0.jpg",
+            "12688094_1.jpg"
+        ],
+        "stock": true
+    }
+];
