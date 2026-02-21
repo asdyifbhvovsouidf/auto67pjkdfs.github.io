8a878878030bc2802b638bb6c1026012aa2b7406
asdyifbhvovsouidf <bvjh56944@gmail.com>
2026-02-22 00:05:20 +0300

    Add files via upload

---
+++ ssss/data/products/chevrolet_malibu.js
@@ -0,0 +1,36 @@
+window['PRODUCTS_CHEVROLET_MALIBU'] = [
+    {
+        "type": "тормозные колодки",
+        "brand": "Chevrolet",
+        "art": "000007",
+        "price": 5000,
+        "desc": "💰Цена за комплект 4 шт. 5000 ₽💰\n\n💯 Оригинал 💯",
+        "images": [
+            "000007_0.jpg"
+        ],
+        "stock": true
+    },
+    {
+        "type": "свечи",
+        "brand": "ACDelco",
+        "art": "12688094",
+        "price": 0,
+        "desc": "Buick encore gx 1.3\nChevrolet malibu 1.3\nChevrolet trailblaizer 1.3\nChevrolet trax 1.3\nOpel Astra K 1.3",
+        "images": [
+            "12688094_0.jpg",
+            "12688094_1.jpg"
+        ],
+        "stock": true
+    },
+    {
+        "type": "Пиропатрон ремня",
+        "brand": "Chevrolet",
+        "art": "000015",
+        "price": 3000,
+        "desc": "Chevrolet malibu \nGmc terrain\nChevrolet equinox",
+        "images": [
+            "000015_0.jpg"
+        ],
+        "stock": true
+    }
+];
---
+++ ssss/data/products/chevrolet_trax.js
@@ -0,0 +1,14 @@
+window['PRODUCTS_CHEVROLET_TRAX'] = [
+    {
+        "type": "свечи",
+        "brand": "ACDelco",
+        "art": "12688094",
+        "price": 0,
+        "desc": "Buick encore gx 1.3\nChevrolet malibu 1.3\nChevrolet trailblaizer 1.3\nChevrolet trax 1.3\nOpel Astra K 1.3",
+        "images": [
+            "12688094_0.jpg",
+            "12688094_1.jpg"
+        ],
+        "stock": true
+    }
+];
